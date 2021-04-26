e7ea751e168ec51bb304737c460433d03b84fdd5
Zamzawi <idhamzamzawi@gmail.com>
2021-04-26 11:26:33 +0700

    Fix carousel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
 
-        if(config('app.env') === 'production') {
+        if(config('app.env') == 'production') {
             \URL::forceScheme('https');
         }
     }
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,25 +4,25 @@
 
 <div class="container mb-7">
     <div class="bd-example mx-2 mb-4">
-        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 @foreach($products as $product)
                 <div class="carousel-item slider {{$product['id']==1? 'active' : ''}}">
                     <a href="detail/{{$product['id']}}">
-                        <img class="img-slider" src="{{$product['image']}}" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>{{$product['name']}}</h5>
-                            <p>{{$product['description']}}</p>
-                        </div>
+                        <img class="mx-auto d-block img-slider" src="{{$product['image']}}" alt="First slide">
                     </a>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>{{$product['name']}}</h5>
+                        <p>{{$product['description']}}</p>
+                    </div>
                 </div>
                 @endforeach
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
